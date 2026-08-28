6368e73a7e595f5afb61ca150e178d4e45b15a52
Zerastos <30709206+zerastos@users.noreply.github.com>
2026-08-28 13:18:59 +0200

    Remove some boilerplate

--- src/main/java/net/slickson/mna_more_structures/Mna_more_structures.java
+++ src/main/java/net/slickson/mna_more_structures/Mna_more_structures.java
@@ -57,22 +57,15 @@ public class Mna_more_structures {
 
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
-        // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
 
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
             event.enqueueWork(() -> {
                 ItemBlockRenderTypes.setRenderLayer(ModBlocks.PROTECTED_IRON_BARS.get(), RenderType.cutout());
             });
